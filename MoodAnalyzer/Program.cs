9632409a1b12115f0ed9bf821d5ed208b20d0d06
zeeshansarwar2968 <zeeshansarwar2968@gmail.com>
2022-06-06 07:26:02 +0530

    [Zeeshan] : Updated main page

--- MoodAnalyzer/Program.cs
+++ MoodAnalyzer/Program.cs
@@ -11,6 +11,7 @@ namespace MoodAnalyzer
             Console.WriteLine("\t\t\t\t    ----------------------------------------");
             Console.WriteLine();
             Console.ForegroundColor = ConsoleColor.Blue;
+
             string message = "I am very sad";
             MoodAnalyserLogic moodAnalyser = new MoodAnalyserLogic(message);
             System.Console.WriteLine("I am in a {0} mood. ",moodAnalyser.AnalyseMood());
